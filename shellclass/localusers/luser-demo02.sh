501eb0d97ff7a8439fb169e2829e8a253aa9fdb6
fforoozan <farazforoozan@gmail.com>
2021-09-13 18:43:46 +0200

    new vagrantfile configuration and localusers bash script

---
+++ shellclass/localusers/luser-demo02.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Display the UID and username of the user executing this script.
+# Display if the user is the root user or not.
+
+# Display the UID
+echo "Your UID is ${UID}"
+
+# Display the username
+USER_NAME=$(id -un)
+echo "Your username is ${USER_NAME}"
+
+if [[ "${UID}" -eq 0 ]]
+then
+  echo 'You are root.'
+else
+  echo 'You are NOT root.'
+fi
