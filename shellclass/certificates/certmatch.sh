8db5fe82b9ed6f9a58fe5f68e14457c51f2eb844
fforoozan <farazforoozan@gmail.com>
2021-10-03 15:00:16 +0200

    Finished certificate matcher

--- shellclass/certificates/certmatch.sh
+++ shellclass/certificates/certmatch.sh
@@ -1,9 +1,10 @@
 #!/bin/bash 
 
-# This script determines certificate files are a match.
+# This script allows users to determine whether certificate files are matching or not by means of comparison of the public key checksums.
 
+# Make sure the user supplies at least one argument. Display help otherwise.
 if [[ "${#}" -lt 1 ]];then
-  echo "Determine certificates are matching or not."
+  echo "This script allows users to determine whether certificate files are matching or not by means of comparison of the public key checksums."
   echo
   echo "Usage:"
   echo "  ${0} [OPTIONS]..."
@@ -17,10 +18,12 @@ if [[ "${#}" -lt 1 ]];then
   exit 1
 fi
 
+# Loop over all positional parameters.
 while [[ "${#}" -gt 0 ]];do
   case "${1}" in
+    # Display help.
     -h|--help)    
-      echo "Determine certificates are matching or not."
+      echo "This script allows users to determine whether certificate files are matching or not by means of comparison of the public key checksums."
       echo
       echo "Usage:"
       echo "  ${0} [OPTIONS]..."
@@ -33,21 +36,25 @@ while [[ "${#}" -gt 0 ]];do
       echo
       exit 0
       ;;
+    # Input parameter public key.
     -p|--public-key)
       shift
       PUBLIC_KEY_FILE="${1}"
       shift
       ;;
+    # Input parameter certificate signing request.
     -c|--csr)
       shift
       CSR_FILE="${1}"
       shift
       ;;
+    # Input parameter private key.
     -k|--private-key)
       shift
       PRIVATE_KEY_FILE="${1}"
       shift
       ;;
+    # For invalid input parameters, display help.
     *)
       echo "Invalid argument: ${1}" 1>&2
       echo
@@ -57,7 +64,10 @@ while [[ "${#}" -gt 0 ]];do
   esac
 done
 
+# Variable containing certificate checksums.
+SUMS=()
 if [[ -f "${PRIVATE_KEY_FILE}" ]];then  
+  # User input passphrase.
   read -s -p "Enter pass phrase for ${PRIVATE_KEY_FILE}: " PASSPHRASE
   if [[ -z "${PASSPHRASE}" ]]; then
     echo 
@@ -65,23 +75,35 @@ if [[ -f "${PRIVATE_KEY_FILE}" ]];then
     exit 1
   fi
   echo  
+  # Attempt to process private key, using passphrase.
   openssl pkey -in "${PRIVATE_KEY_FILE}" -passin pass:"${PASSPHRASE}" -pubout -outform pem &>/dev/null 
   if [[ "${?}" -eq 1 ]];then
     echo "invalid passphrase" 1>&2
     exit 1
   fi
-  PRIVATE_SUM=$(openssl pkey -in "${PRIVATE_KEY_FILE}" -passin pass:"${PASSPHRASE}" -pubout -outform pem |& sha256sum)
+  # Get private key checksum and append to array.
+  PRIVATE_SUM=$(openssl pkey -in "${PRIVATE_KEY_FILE}" -passin pass:"${PASSPHRASE}" -pubout -outform pem | sha256sum)
+  SUMS+=("${PRIVATE_SUM}")
 fi
 
 if [[ -f "${PUBLIC_KEY_FILE}" ]];then
-  PUBLIC_SUM=$(openssl x509 -in "${PUBLIC_KEY_FILE}" -pubkey -noout -outform pem |& sha256sum)
+  # Get public key checksum and append to array.
+  PUBLIC_SUM=$(openssl x509 -in "${PUBLIC_KEY_FILE}" -pubkey -noout -outform pem | sha256sum)
+  SUMS+=("${PUBLIC_SUM}")
 fi 
 
 if [[ -f "${CSR_FILE}" ]];then
-  CSR_SUM=$(openssl req -in "${CSR_FILE}" -pubkey -noout -outform pem |& sha256sum)
-fi 
+  # Get csr checksum and append to array.
+  CSR_SUM=$(openssl req -in "${CSR_FILE}" -pubkey -noout -outform pem | sha256sum)
+  SUMS+=("${CSR_SUM}")
+fi
 
-for i in "${PRIVATE_SUM}" "${PUBLIC_SUM}" "${CSR_SUM}";do
-  if [[ -n "${i}" ]];then
+LENGTH="${#SUMS[@]}"
+for (( i=1; i<"${LENGTH}"; i++ ));do
+  if [[ "${SUMS[0]}" != "${SUMS[$i]}" ]];then
+    echo "no match" 
+    exit 1
   fi
 done
+echo "match"
+exit 0
